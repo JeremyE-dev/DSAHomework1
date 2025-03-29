6a8294cb3a414e07c8af5e43f9fdb8f061a9fb21
JeremyE-Dev <eudaly_19@hotmail.com>
2025-03-29 11:55:19 -0500

    initial file setup

---
+++ DSAHomework1/Homework1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAHomework1
+{
+    class Homework1
+    {
+        // returns a new array populated with contents of original array
+        // with the given value inserted at the given index
+        // edge cases: 
+        // ** array is null or empty -- "error - array must have at least one value"
+        // ** array has one value
+        // ** index is negative -- "error - index cannot be negative"
+        // ** index is zero -- insert at beginning of the array
+        // ** index is greater than the length of the array -- insert at the end of the array
+        public static int[] Insert(int[] array, int index, int value)
+        {
+            //edge cases
+            // array is null
+            if (array == null )
+            {
+                throw new ArgumentException("input array cannot be null");
+            }
+            
+            // create new array one larger than original
+            int[] newArray = new int[array.Length + 1];
+
+            // copy elements up to insert point from original array to new array
+
+            return null;
+
+        }
+    }
+}
---
+++ DSAHomework1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DSAHomework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 
+            try
+            {
+                int[] result = Homework1.Insert(null,0,0);
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Error:{0}", ex.Message);
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+           
+        }
+    }
+}
